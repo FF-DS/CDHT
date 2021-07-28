24e4dcf9cad8c40766d256fda1de82ed287fedc8
MohammedAlewi <rofyalewi@gmail.com>
2021-07-28 14:56:00 +0300

    [cdht] adding ping server...

--- CDHT/main.go
+++ CDHT/main.go
@@ -9,31 +9,57 @@ import (
     "bytes"
     "encoding/json"
     "crypto/sha1"
+    "net"  
+    "bufio"
 )
 
 
 type NodeData struct {
-    _id string
-    node_id string
-    iP_address string
-    created_date string
+    ID string
+    Node_id string
+    IP_address string
+    Created_date string
 }
 
-type Nodes []NodeData 
-
+func getUserInput() string {
+    fmt.Println("Options ")
+    fmt.Println("-----------------------------------")
+    fmt.Println("[1] for registering your Node")
+    fmt.Println("[2] for getting list of Nodes")
+    fmt.Println("[3] for pinging a node")
+    fmt.Println("-----------------------------------")
+    fmt.Print("Enter your option :")
+    var userInput string
+    fmt.Scanln(&userInput)
+    return userInput
+}
 
 func main() {
 
+    fmt.Print("Enter Computer IP: ")
+    var IpAddress string
+    fmt.Scanln(&IpAddress)
+
+    go pingServer(IpAddress)
+
     for {
 
-        go gettingRegisteredNodes()
-        go registerNode()
+        userInput := getUserInput()
+        switch userInput {
+            case "1":
+                go registerNode(IpAddress)
+            case "2":
+                go gettingRegisteredNodes()
+            case "3":
+                go pingClient()
+
+        }
 
         time.Sleep(time.Minute)
     }
+
     
 }
-
 
 func gettingRegisteredNodes() {
     resp, err := http.Get("https://cdht-monitoring-api.herokuapp.com/nodes")
@@ -48,25 +74,36 @@ func gettingRegisteredNodes() {
     }
 
     fmt.Println("\n\n[NODE]: Receiving Nodes...")
-    fmt.Println("-----------------------------------------\n")
+    fmt.Println("-----------------------------------------")
     
     
-    stringRep := string(body)
     
-    messages := []NodeData{} 
-    json.Unmarshal(body, &messages)
-    fmt.Println(messages)
-    fmt.Println(stringRep)
+    var messages []NodeData
+    err = json.Unmarshal(body, &messages)
+    
+    if err != nil {
+		fmt.Println("error:", err)
+	}
+
+    visited := map[string]bool{}
+
+    for _, node := range messages {
+        if _,exitsts := visited[node.IP_address]; !exitsts {
+            fmt.Println("[NODE-IP] "+ node.IP_address)
+            visited[node.IP_address] = true
+        }
+    }
 }
 
 
-func registerNode() {
+func registerNode(ipAddress string) {
     nodeID := sha1.New()
     nodeID.Write([]byte( time.Now().String()  ))
     node_id := nodeID.Sum(nil)
 
     postBody, _ := json.Marshal(map[string]string{
         "Node_id": string( node_id ),
+        "IP_address": string( ipAddress ),
     })
 
     responseBody := bytes.NewBuffer(postBody)
@@ -74,17 +111,65 @@ func registerNode() {
     
     if err != nil {
        log.Fatalf("An Error Occured %v", err)
+       return
     }
     
     defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-       log.Fatalln(err)
-    }
     
     fmt.Println("\n\n[NODE]: Registering Node...")
-    fmt.Println("-----------------------------------------\n")
-    fmt.Println(string(body))
+    fmt.Println("-----------------------------------------")
+    fmt.Println("Node registered successfully.")
 }
 
+
+
+func pingServer(IpAddress string) {
+    p := make([]byte, 2048)
+    addr := net.UDPAddr{
+        Port: 1234,
+        IP: net.ParseIP(IpAddress),
+    }
+    fmt.Printf("[PING] ping server running... \n")
+    ser, err := net.ListenUDP("udp", &addr)
+    if err != nil {
+        fmt.Printf("Some error %v\n", err)
+        return
+    }
+    for {
+        _,remoteaddr,err := ser.ReadFromUDP(p)
+        fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+        if err !=  nil {
+            fmt.Printf("Some error  %v", err)
+            continue
+        }
+
+        _,err   = ser.WriteToUDP([]byte("From server: Hello I got your message "), remoteaddr)
+        if err != nil {
+            fmt.Printf("Couldn't send response %v", err)
+        }
+    }
+}
+
+
+
+func pingClient(){
+    fmt.Print("\n\nEnter Node IP to ping: ")
+    var IpAddress string
+    fmt.Scanln(&IpAddress)
+    fmt.Print("\n")
+
+    p :=  make([]byte, 2048)
+    conn, err := net.Dial("udp", IpAddress+ ":1234")
+    if err != nil {
+        fmt.Printf("Some error %v", err)
+        return
+    }
+    fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
+    _, err = bufio.NewReader(conn).Read(p)
+    if err == nil {
+        fmt.Printf("%s\n", p)
+    } else {
+        fmt.Printf("Some error %v\n", err)
+    }
+    conn.Close()
+}
