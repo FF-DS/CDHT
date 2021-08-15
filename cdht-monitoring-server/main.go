75a56d0426059aeadbe39204a05147e4eac202dc
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2021-08-15 12:02:37 +0300

    [cdht-api] addiing middle-ware

--- cdht-monitoring-server/main.go
+++ cdht-monitoring-server/main.go
@@ -18,6 +18,7 @@ func init() {
 func main() {
     // Init gin router
     router := gin.Default()
+    router.Use(CORSMiddleware());
 	routers.NodesRoute(router) 
 
     // Handle error response when a route is not defined
@@ -27,4 +28,21 @@ func main() {
 
     // Init our server
     router.Run( ":" + os.Getenv("PORT") )
+}
+
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
