085aa1a3c0c093a543200521db0b1c313267a28e
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2021-09-14 16:07:53 +0300

    [cdht-monitoring] finishing end points

--- cdht-monitoring-server/controllers/commandDispatcherControllelr.go
+++ cdht-monitoring-server/controllers/commandDispatcherControllelr.go
@@ -1,7 +1,16 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
+	. "monitoring-server/services"
+	"monitoring-server/core"
+    "github.com/gin-gonic/gin"
+	"context"
+    "log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+    // "go.mongodb.org/mongo-driver/bson/primitive"
+    // "time"
+	"net/http"
 )
 
 type CommandDispatcherController struct{}
@@ -9,37 +18,154 @@ type CommandDispatcherController struct{}
 /* 
 This function will return a specified number of pending commands from the command collections
 */
-func (command CommandDispatcherController) GetPendingCommands(c *gin.Context){
+func (self CommandDispatcherController) GetPendingCommands(c *gin.Context){
 
+	findOptions := options.Find()
+	findOptions.SetLimit(20)
+
+	commandCollection := ConnectDB("commands")
+
+	cursor , err := commandCollection.Find(context.TODO() , bson.M{} , findOptions )
+
+	if err != nil{
+		GetError(err , c)
+		return
+	}
+
+	defer cursor.Close(context.TODO())
+    var commands []core.Command
+
+
+    for cursor.Next(context.TODO()) {
+		var command core.Command
+		err := cursor.Decode(&command)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		commands = append(commands, command)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+ 
+    c.JSON(http.StatusOK, gin.H{"message": "Pending commands retrived successfully", "data": commands} )
 }
-
 
 /* 
 This function will add a command in to the command collection
 */
-func (command CommandDispatcherController) AddPendingCommand(c *gin.Context){
+func (self CommandDispatcherController) AddPendingCommand(c *gin.Context){
+	commandCollection := ConnectDB("commands")
 
+	var command core.Command
+
+	if err := c.ShouldBindJSON(&command) ; err == nil{
+	
+        result, err := commandCollection.InsertOne(context.TODO(), command)
+    
+        if err != nil {
+            GetError(err, c)
+        }
+    
+        c.JSON(http.StatusOK, gin.H{"message": "Commands queued successfully" , "data" : result})
+    } else {
+        c.JSON(401, gin.H{"error": err.Error()})
+    }
+}
+
+
+/* 
+This function will add a batch of commands in to the command collection
+*/
+func (self CommandDispatcherController) AddPendingCommandsByBatch(c *gin.Context){
+	commandCollection := ConnectDB("commands")
+
+	var commands []interface{}
+
+	if err := c.ShouldBindJSON(&commands) ; err == nil{
+	
+        result, err := commandCollection.InsertMany(context.TODO(), commands)
+    
+        if err != nil {
+            GetError(err, c)
+        }
+    
+        c.JSON(http.StatusOK, gin.H{"message": "Commands queued successfully" , "data" : result})
+    } else {
+        c.JSON(401, gin.H{"error": err.Error()})
+    }
 }
 
 /* 
 This function will return results for a specific command
 */
-func (command CommandDispatcherController) GetCommandResultReports(c *gin.Context){
+func (self CommandDispatcherController) GetCommandResultReports(c *gin.Context){
+
+	findOptions := options.Find()
+	findOptions.SetLimit(20)
+
+	commandResultsCollection := ConnectDB("command-results")
+
+	cursor , err := commandResultsCollection.Find(context.TODO() , bson.M{} , findOptions )
+
+	if err != nil{
+		GetError(err , c)
+		return
+	}
+
+	defer cursor.Close(context.TODO())
+    var commandResults []core.CommandResult
+
+
+    for cursor.Next(context.TODO()) {
+		var commandRes core.CommandResult
+		err := cursor.Decode(&commandRes)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		commandResults = append(commandResults, commandRes)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+ 
+    c.JSON(http.StatusOK, gin.H{"message": "Command Results retrived successfully", "data": commandResults} )
 
 }
 
 /* 
 This function will add an intermideate result into the command_response collection
 */
-func (command CommandDispatcherController) AddCommandResponseReport(c *gin.Context){
+func (self CommandDispatcherController) AddCommandResponseReport(c *gin.Context){
+	commandResultsCollection := ConnectDB("command-results")
 
+	var commandRes core.CommandResult
+
+	if err := c.ShouldBindJSON(&commandRes) ; err == nil{
+	
+        result, err := commandResultsCollection.InsertOne(context.TODO(), commandRes)
+    
+        if err != nil {
+            GetError(err, c)
+        }
+    
+        c.JSON(http.StatusOK, gin.H{"message": "Command Result added successfully" , "data" : result})
+    } else {
+        c.JSON(401, gin.H{"error": err.Error()})
+    }
 }
 
 
 /* 
 This function will clear the commands collection , thread lightly!!!!!!!!!!!!
 */
-func (command CommandDispatcherController) ClearCommandsCollection(c *gin.Context){
+func (self CommandDispatcherController) ClearCommandsCollection(c *gin.Context){
 
 }
 
@@ -47,6 +173,6 @@ func (command CommandDispatcherController) ClearCommandsCollection(c *gin.Contex
 /* 
 This function will clear the command_results collection , thread lightly!!!!!!!!!!!!
 */
-func (command CommandDispatcherController) ClearCommandResultsCollection(c *gin.Context){
+func (self CommandDispatcherController) ClearCommandResultsCollection(c *gin.Context){
 
 }
