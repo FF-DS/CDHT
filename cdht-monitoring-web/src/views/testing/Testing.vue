ca22ac7d78e31b35622b24ca6b64317b62594f9c
MohammedAlewi <rofyalewi@gmail.com>
2021-09-28 07:30:41 +0300

    [cdht-web] adding component

--- cdht-monitoring-web/src/views/testing/Testing.vue
+++ cdht-monitoring-web/src/views/testing/Testing.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="my-5">
     <v-layout row>
-      <v-flex xs12 sm6>
+      <v-flex xs12 sm5>
         <v-layout row class="mx-2">
           <v-card elevation="2" outlined shaped tile>
             <v-divider></v-divider>
@@ -9,7 +9,7 @@
               Run Test tools on a node
             </div>
             <v-divider></v-divider>
-            <v-card elevation="3" tile>
+            <v-card elevation="3" tile class="my-3">
               <v-card-title>Ping Tool</v-card-title>
               <v-text-field
                 label="Node ID"
@@ -34,23 +34,271 @@
                 </v-btn></v-card-actions
               >
             </v-card>
-            <v-card class="my-2" elevation="3" tile>
-              <v-card-title>Card title</v-card-title>
-              <v-card-subtitle>Card sub title</v-card-subtitle>
-              <v-card-actions>Action 1</v-card-actions>
+            <v-card elevation="3" tile class="my-3">
+              <v-card-title>DNS lookup</v-card-title>
+              <v-text-field
+                label="Node ID"
+                v-model="lookupNodeId"
+                :rules="pingNodeIdRules"
+                placeholder="2"
+                class="mx-2"
+                outlined
+              ></v-text-field>
+
+              <v-card-actions
+                ><v-btn
+                  color="success"
+                  elevation="2"
+                  class="mr-4"
+                  @click="runPing"
+                >
+                  Run DNS lookup
+                  <v-icon right dark>
+                    mdi-subdirectory-arrow-right
+                  </v-icon>
+                </v-btn></v-card-actions
+              >
             </v-card>
-            <v-card class="my-2" elevation="3" tile>
-              <v-card-title>Card title</v-card-title>
-              <v-card-subtitle>Card sub title</v-card-subtitle>
-              <v-card-actions>Action 1</v-card-actions>
+            <v-card elevation="3" tile class="my-3">
+              <v-card-title>Hop Count Tool</v-card-title>
+              <v-text-field
+                label="Start Node ID"
+                v-model="hopNodeId1"
+                :rules="pingNodeIdRules"
+                placeholder="2"
+                class="mx-2 my-0"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                label="Destination Node ID"
+                v-model="hopNodeId2"
+                :rules="pingNodeIdRules"
+                placeholder="2"
+                class="mx-2 my-0"
+                outlined
+              ></v-text-field>
+
+              <v-card-actions
+                ><v-btn
+                  color="success"
+                  elevation="2"
+                  class="mr-4"
+                  @click="runPing"
+                >
+                  Run Hop Count
+                  <v-icon right dark>
+                    mdi-subdirectory-arrow-right
+                  </v-icon>
+                </v-btn></v-card-actions
+              >
             </v-card>
           </v-card>
         </v-layout>
       </v-flex>
-      <v-flex xs12 sm6>
-        <v-layout row class="mx-2">
-          <v-divider></v-divider>
-        </v-layout>
+      <v-flex xs12 sm7>
+        <v-card class="mx-0" elevation="2">
+          <div class="">
+            <v-layout row class="mx-2" fluid>
+              <v-container class="ma-0 pa-0">
+                <v-divider></v-divider>
+                <div class="text-darken-1 mb-2 h4">
+                  Response log from the node
+                </div>
+                <v-divider></v-divider>
+                <div
+                  class=""
+                  v-for="(report, index) in logReports"
+                  v-bind:key="index"
+                >
+                  <v-card
+                    elevation="2"
+                    v-if="report.Type == 'COMMAND_TYPE_PING'"
+                  >
+                    <v-layout class="my-1 pa-3">
+                      <v-flex xs6>
+                        <span>Command executor Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Log Type :</span>
+                                {{ report.Type }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node ID :</span>
+                                {{ report.NodeId }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Address :</span>
+                                {{ report.NodeAddress }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray"
+                                  >Operation Status :</span
+                                >
+                                {{ report.OperationStatus }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                      <v-divider vertical></v-divider>
+                      <v-flex xs5>
+                        <span>Remote Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Status :</span>
+                                {{ report.Body.rtt }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node id :</span>
+                                {{ report.Body.Node_id }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Address :</span>
+                                {{ report.Body.Node_Address }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">RTT :</span>
+                                {{ report.Body.rtt }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                    </v-layout>
+                  </v-card>
+
+                  <v-card
+                    elevation="2"
+                    v-if="report.Type == 'COMMAND_TYPE_HOP_COUNT'"
+                  >
+                    <v-layout class="my-1 pa-3">
+                      <v-flex xs6>
+                        <span>Command executor Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Log Type :</span>
+                                {{ report.Type }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node ID :</span>
+                                {{ report.NodeId }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Address :</span>
+                                {{ report.NodeAddress }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray"
+                                  >Operation Status :</span
+                                >
+                                {{ report.OperationStatus }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                      <v-divider vertical></v-divider>
+                      <v-flex xs5>
+                        <span>Remote Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Status :</span>
+                                {{ report.Body.rtt }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Hop length :</span>
+                                {{ report.Body.length }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">RTT :</span>
+                                {{ report.Body.rtt }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                    </v-layout>
+                  </v-card>
+
+                  <v-card
+                    elevation="2"
+                    v-if="report.Type == 'COMMAND_TYPE_LOOK_UP'"
+                  >
+                    <v-layout class="my-1 pa-3">
+                      <v-flex xs6>
+                        <span>Command executor Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Log Type :</span>
+                                {{ report.Type }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node ID :</span>
+                                {{ report.NodeId }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Address :</span>
+                                {{ report.NodeAddress }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray"
+                                  >Operation Status :</span
+                                >
+                                {{ report.OperationStatus }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                      <v-divider vertical></v-divider>
+                      <v-flex xs5>
+                        <span>Remote Node Info</span>
+
+                        <v-card-text>
+                          <v-list-item two-line>
+                            <v-list-item-content>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node M :</span>
+                                {{ report.Body.NodeM }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node id :</span>
+                                {{ report.Body.NodeId }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">Node Address :</span>
+                                {{ report.Body.NodeAddress }}
+                              </v-list-item-subtitle>
+                              <v-list-item-subtitle>
+                                <span class="text--gray">RTT :</span>
+                                {{ report.Body.rtt }}
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-card-text>
+                      </v-flex>
+                    </v-layout>
+                  </v-card>
+                </div>
+              </v-container>
+            </v-layout>
+          </div>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -65,6 +313,70 @@ export default {
   data: () => {
     return {
       pingNodeId: null,
+      lookupNodeId: null,
+      hopNodeId1: null,
+      hopNodeId2: null,
+      logReports: [
+        {
+          Type: "COMMAND_TYPE_PING",
+          NodeId: "NodeId",
+          NodeAddress: "NodeAddress",
+          OperationStatus: "OperationStatus",
+          Body: {
+            rtt: "rtt",
+            Node_Status: "Node_Status",
+            Node_id: "Node_id",
+            Node_Address: "Node_Address",
+          },
+        },
+        {
+          Type: "COMMAND_TYPE_HOP_COUNT",
+          NodeId: "NodeId",
+          NodeAddress: "NodeAddress",
+          OperationStatus: "OperationStatus",
+          Body: {
+            hops: "rtt",
+            rtt: "Node_Status",
+            length: "length",
+          },
+        },
+        {
+          Type: "COMMAND_TYPE_LOOK_UP",
+          NodeId: "NodeId",
+          NodeAddress: "NodeAddress",
+          OperationStatus: "OperationStatus",
+          Body: {
+            NodeId: "NodeId",
+            NodeAddress: "NodeAddress",
+            NodeM: "NodeM",
+            rtt: "rtt",
+          },
+        },
+        {
+          Type: "COMMAND_TYPE_LOOK_UP",
+          NodeId: "NodeId",
+          NodeAddress: "NodeAddress",
+          OperationStatus: "OperationStatus",
+          Body: {
+            NodeId: "NodeId",
+            NodeAddress: "NodeAddress",
+            NodeM: "NodeM",
+            rtt: "rtt",
+          },
+        },
+        {
+          Type: "COMMAND_TYPE_LOOK_UP",
+          NodeId: "NodeId",
+          NodeAddress: "NodeAddress",
+          OperationStatus: "OperationStatus",
+          Body: {
+            NodeId: "NodeId",
+            NodeAddress: "NodeAddress",
+            NodeM: "NodeM",
+            rtt: "rtt",
+          },
+        },
+      ],
       pingNodeIdRules: [
         (v) => !isNaN(parseFloat(v)) || "It has to be a Number",
       ],
